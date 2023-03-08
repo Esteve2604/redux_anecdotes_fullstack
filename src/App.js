44e14ba8d34f58e6488f5b9905bcbb34822b3c2e
Esteve2604 <estebanasperuiz@gmail.com>
2023-03-08 20:52:12 +0100

    Parte 6.15

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteFilter from './components/AnecdoteFilter'
 import Notification from './components/Notification'
+import { setBlogs } from './reducers/anecdoteReducer'
+import { useEffect } from 'react'
+import anecdotesService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
 const App = () => {
-
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    anecdotesService.getAll().then(blogs=>dispatch(setBlogs(blogs)))
+  },[dispatch])
   return (
     <div>
       <AnecdoteFilter />
--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,13 +1,16 @@
 import { useDispatch } from 'react-redux'
-import { addAnecdote } from '../reducers/anecdoteReducer'
+import { appendBlog } from '../reducers/anecdoteReducer'
+import anecdotesService from '../services/anecdotes'
+
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const newAnecdote = (event) => {
+    const newAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(addAnecdote(content))
+        const newBlog= await anecdotesService.newBlog(content)
+        dispatch(appendBlog(newBlog))
     }
     return <>
         <h2>create new</h2>
